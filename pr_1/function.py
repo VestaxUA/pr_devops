ec279e254f13e00edf88a0ef8d1fa5e26a9b5e9b
VestaxUA <valiktkachishen@gmail.com>
2025-09-12 08:52:51 +0300

    додано виведення списку товарів в function.py

--- pr_1/function.py
+++ pr_1/function.py
@@ -22,11 +22,26 @@ calculate_stock_value(products)
 
 def calculate_discount(product_info):
     for product, details in product_info.items():
-        if details['Залишок'] < 10:
+        if details['Кількість'] < 10:
             discount = details['Ціна'] * 0.95   # 5% знижка
             print(f"Знижка для {product}: {discount} ({details['Ціна']} - 5%)")
         else:
             print(f"Для {product} знижка не застосовується, оскільки залишок становить {details['Кількість']} одиниць.")
 
 calculate_discount(products)
+
+def print_product_names(product_names, index=0):
+    # Перевіряємо, чи індекс не виходить за межі списку
+    if index < len(product_names):
+        # Виводимо назву товару за поточним індексом
+        print(product_names[index])
+        # Рекурсивно викликаємо функцію з наступним індексом
+        print_product_names(product_names, index + 1)
+
+
+# Отримуємо список назв товарів
+product_names = list(products.keys())
+# Викликаємо рекурсивну функцію для виведення назв товарів
+print_product_names(product_names)
+
 
