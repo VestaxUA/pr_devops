56a31b4e3c1f516e0b313c0adc6be59eb20360a0
VestaxUA <valiktkachishen@gmail.com>
2025-09-12 08:58:04 +0300

    додано пошук товарів в function.py

--- pr_1/function.py
+++ pr_1/function.py
@@ -45,3 +45,26 @@ product_names = list(products.keys())
 print_product_names(product_names)
 
 
+def find_product_by_name(products, product_name, index=0):
+    # Перетворюємо ключі словника у список для рекурсивного пошуку
+    product_keys = list(products.keys())
+
+    # Умова завершення рекурсії
+    if index >= len(product_keys):
+        print("Товар не знайдено.")
+        return
+
+    # Перевіряємо, чи поточний ключ відповідає шуканому товару
+    if product_keys[index] == product_name:
+        product_info = products[product_name]
+        print(f"Товар знайдено: {product_name}, Ціна: {product_info['Ціна']}, Кількість: {product_info['Кількість']}")
+        return
+
+    # Рекурсивний виклик функції з наступним індексом
+    find_product_by_name(products, product_name, index + 1)
+
+
+find_product_by_name(products, "Товар2")
+
+
+
