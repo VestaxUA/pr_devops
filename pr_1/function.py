4d0c79d64e8650236b743ecddf49c229656eb1a9
VestaxUA <valiktkachishen@gmail.com>
2025-09-12 08:44:03 +0300

    додано розрахунок знижки в function.py + виправлення попереджень

--- pr_1/function.py
+++ pr_1/function.py
@@ -15,7 +15,18 @@ print(products)
 
 def calculate_stock_value(product_info):
     for product, details in product_info.items():
-        stock_value = product.price * product.stock;
-        print(f"Вартість залишку для {product.name} : {stock_value}")
+        stock_value = product.price * product.stock
+        print(f"Вартість залишку для {product} : {stock_value}")
 
 calculate_stock_value(products)
+
+def calculate_discount(product_info):
+    for product, details in product_info.items():
+        if details['Залишок'] < 10:
+            discount = product.price * 0.95   # 5% знижка
+            print(f"Знижка для {product}: {discount} ({details['Ціна']} - 5%)")
+        else:
+            print(f"Для {product} знижка не застосовується, оскільки залишок становить {details['Кількість']} одиниць.")
+
+calculate_discount(products)
+
