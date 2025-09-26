b9cee6ad2cbc4628b3f5e78a413e92ddde06baa6
VestaxUA <valiktkachishen@gmail.com>
2025-09-26 08:51:31 +0300

    модифікація main.py для роботи з StudentFinder.py

--- pr_2/main.py
+++ pr_2/main.py
@@ -1,6 +1,7 @@
 from CsvReader import CsvReader
 from CsvToJsonConverter import CsvToJsonConverter
 from JsonReader import JsonReader
+from pr_2.StudentFinder import StudentFinder
 
 """if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
@@ -19,6 +20,14 @@ if __name__ == "__main__":
 
     # Читання та відображення даних з JSON
     json_reader = JsonReader()
-    json_reader.display_data(json_path)
+    #json_reader.display_data(json_path)
+
+    # Пошук та виведення інформації про студентів
+    finder = StudentFinder(json_reader)
+    finder.load_data(json_path)
+    surname_to_find = "Барченко"  # Вкажіть прізвище для пошуку
+    found_students = finder.find_students_by_surname(surname_to_find)
+    finder.display_students_info(found_students)
+
 
 
