8104828817de4c394cc631511377b6d78a0eae89
VestaxUA <valiktkachishen@gmail.com>
2025-10-10 08:34:00 +0300

    розширення класу Catcher.py

--- pr_3/Catcher.py
+++ pr_3/Catcher.py
@@ -7,3 +7,18 @@ class Catcher:
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
 
+        # Прив'язка методів до подій клавіатури
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def turn_left(self, evt):
+        # Якщо ловець не на краю зліва, встановлюємо швидкість для руху вліво
+        if self.canvas.coords(self.id)[0] > 0:
+            self.x = -20
+
+    def turn_right(self, evt):
+        # Якщо ловець не на краю справа, встановлюємо швидкість для руху вправо
+        if self.canvas.coords(self.id)[2] >= self.canvas_width:
+            self.x = 20
+
+
