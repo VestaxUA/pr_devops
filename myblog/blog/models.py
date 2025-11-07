e87cf5e4dd66845af8cb119537b49cc1dd41dd08
VestaxUA <valiktkachishen@gmail.com>
2025-11-07 08:49:07 +0200

    створення модельного менеджеру в models.py

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -20,6 +24,8 @@ class Post(models.Model):
         default=Status.DRAFT
     )
 
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
