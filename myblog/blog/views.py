e31eac73e64ddac866e297c5057ddfdb1acfe475
VestaxUA <valiktkachishen@gmail.com>
2025-11-21 12:14:57 +0200

    методи для надсилання листів електронною поштою

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -40,14 +41,21 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            # Надіслати електронний лист з постом
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
 
 
 
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -121,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Конфігурація сервера електронної пошти
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'valiktkachishen@gmail.com'
+EMAIL_HOST_PASSWORD = 'testpass'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
