f1bc019ea53c452dadee10f3dd0986bcd468762a
VestaxUA <valiktkachishen@gmail.com>
2025-11-21 12:09:06 +0200

    створення методу post_share в views.py

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -36,6 +37,18 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # Надіслати електронний лист з постом
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
 
 
 
